4502c322bdbb3edcd7554f46ff60ec5612eafd46
Satendra1916 <sp4rsa@gmail.com>
2025-02-23 18:47:02 +0530

    add full name

--- ss.java
+++ ss.java
@@ -4,6 +4,6 @@ class SS
     public static void main(String[] arg)
     {
         JOptionPane.showMessageDialog(null,"SPatel");
-        JOptionPane.showMessageDialog(null, "SAtendra");
+        JOptionPane.showMessageDialog(null, "Satendra Patel");
     }
 }
