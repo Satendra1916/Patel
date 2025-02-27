d11d9a548cef8f3144b3d054dc1aaf768a2ee359
Satendra1916 <sp4rsa@gmail.com>
2025-02-27 18:53:14 +0530

    remove

--- ss.java
+++ ss.java
@@ -3,9 +3,10 @@ class SS
 {
     public static void main(String[] arg)
     {
+
         JOptionPane.showMessageDialog(null,"SPatel");
-        JOptionPane.showMessageDialog(null, "Satendra Patel");
-        JOptionPane.showMessageDialog(null, "Satendra19 Patel");
+        
+        
 
     }
 }
